864d661a388508a156f1eb228d323950e69be4c4
Pavan Lal Boro <pavan.boro@resideo.com>
2019-11-15 11:42:56 +0530

    placed 'proceed' buttom at bottom

--- Resideo_eshopping/resideo_eshopping/lib/pages/addUserDetails.dart
+++ Resideo_eshopping/resideo_eshopping/lib/pages/addUserDetails.dart
@@ -57,128 +57,135 @@ class AddUserDetails extends StatelessWidget {
         child: Form(
           key: _formKeyValue,
           autovalidate: true,
-          child: new ListView(
-            padding: const EdgeInsets.symmetric(horizontal: 15.0),
+          child: Column(
             children: <Widget>[
-              TextFormField(
-                decoration: const InputDecoration(
-                  icon: const Icon(
-                    FontAwesomeIcons.userCircle,
-                    color: Colors.blue,
-                  ),
-                  hintText: 'Enter your Name',
-                  labelText: 'Name',
-                ),
-                validator: (val) {
-                  if (val.isEmpty) {
-                    return 'Name field should not be empty';
-                  } else {
-                    return null;
-                  }
-                },
-              ),
-              TextFormField(
-                decoration: const InputDecoration(
-                  icon: const Icon(
-                    FontAwesomeIcons.phone,
-                    color: Colors.blue,
-                  ),
-                  hintText: 'Ex: 9700000000',
-                  labelText: 'Phone',
-                ),
-                validator: (val) {
-                  Pattern pattern = r'^(?:[+0]9)?[0-9]{10}$';
-                  RegExp regex = new RegExp(pattern);
-                  if (!regex.hasMatch(val))
-                    return 'Enter Valid Phone Number';
-                  else
-                    return null;
-                },
-                keyboardType: TextInputType.number,
-              ),
-              TextFormField(
-                maxLines: 3,
-                decoration: const InputDecoration(
-                  icon: const Icon(
-                    FontAwesomeIcons.home,
-                    color: Colors.blue,
-                  ),
-                  labelText: 'Address',
-                ),
-                validator: (val) {
-                  if (val.isEmpty)
-                    return 'Address Field should not be empty';
-                  else
-                    return null;
-                },
-              ),
-              TextFormField(
-                decoration: const InputDecoration(
-                  icon: const Icon(
-                    FontAwesomeIcons.mapMarker,
-                    color: Colors.blue,
-                  ),
-                  hintText: 'Ex: 500000',
-                  labelText: 'Zip Code',
-                ),
-                validator: (val) {
-                  Pattern pattern = r'^[1-9][0-9]{5}$';
-                  RegExp regex = new RegExp(pattern);
-                  if (!regex.hasMatch(val))
-                    return 'Enter Valid ZipCode';
-                  else
-                    return null;
-                },
-                keyboardType: TextInputType.number,
-              ),
-              Row(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: <Widget>[
-                  Container(
-                    margin: const EdgeInsets.all(20.0),
-                    child: Text(
-                      'Estimated Price: ',
-                      style: TextStyle(
-                        fontWeight: FontWeight.bold,
-                        fontSize: 20,
-                        color: Colors.blue,
+              Flexible(
+                child: ListView(
+                  padding: const EdgeInsets.symmetric(horizontal: 15.0),
+                  children: <Widget>[
+                    TextFormField(
+                      decoration: const InputDecoration(
+                        icon: const Icon(
+                          FontAwesomeIcons.userCircle,
+                          color: Colors.blue,
+                        ),
+                        hintText: 'Enter your Name',
+                        labelText: 'Name',
                       ),
+                      validator: (val) {
+                        if (val.isEmpty) {
+                          return 'Name field should not be empty';
+                        } else {
+                          return null;
+                        }
+                      },
                     ),
-                  ),
-                  Container(
-                    margin: const EdgeInsets.all(20.0),
-                    child: Text(
-                      'Rs. ' + estimatedCost.toString(),
-                      style:
-                          TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
+                    TextFormField(
+                      decoration: const InputDecoration(
+                        icon: const Icon(
+                          FontAwesomeIcons.phone,
+                          color: Colors.blue,
+                        ),
+                        hintText: 'Ex: 9700000000',
+                        labelText: 'Phone',
+                      ),
+                      validator: (val) {
+                        Pattern pattern = r'^(?:[+0]9)?[0-9]{10}$';
+                        RegExp regex = new RegExp(pattern);
+                        if (!regex.hasMatch(val))
+                          return 'Enter Valid Phone Number';
+                        else
+                          return null;
+                      },
+                      keyboardType: TextInputType.number,
                     ),
-                  ),
-                ],
-              ),
-              SizedBox(
-                height: 200.0,
+                    TextFormField(
+                      maxLines: 3,
+                      decoration: const InputDecoration(
+                        icon: const Icon(
+                          FontAwesomeIcons.home,
+                          color: Colors.blue,
+                        ),
+                        labelText: 'Address',
+                      ),
+                      validator: (val) {
+                        if (val.isEmpty)
+                          return 'Address Field should not be empty';
+                        else
+                          return null;
+                      },
+                    ),
+                    TextFormField(
+                      decoration: const InputDecoration(
+                        icon: const Icon(
+                          FontAwesomeIcons.mapMarker,
+                          color: Colors.blue,
+                        ),
+                        hintText: 'Ex: 500000',
+                        labelText: 'Zip Code',
+                      ),
+                      validator: (val) {
+                        Pattern pattern = r'^[1-9][0-9]{5}$';
+                        RegExp regex = new RegExp(pattern);
+                        if (!regex.hasMatch(val))
+                          return 'Enter Valid ZipCode';
+                        else
+                          return null;
+                      },
+                      keyboardType: TextInputType.number,
+                    ),
+                    Row(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: <Widget>[
+                        Container(
+                          margin: const EdgeInsets.all(20.0),
+                          child: Text(
+                            'Estimated Price: ',
+                            style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                              fontSize: 20,
+                              color: Colors.blue,
+                            ),
+                          ),
+                        ),
+                        Container(
+                          margin: const EdgeInsets.all(20.0),
+                          child: Text(
+                            'Rs. ' + estimatedCost.toString(),
+                            style: TextStyle(
+                                fontWeight: FontWeight.bold, fontSize: 20),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
               ),
               Column(
                 crossAxisAlignment: CrossAxisAlignment.stretch,
                 children: <Widget>[
-                  RaisedButton(
-                      color: Colors.blue,
-                      textColor: Colors.white,
-                      child: Padding(
-                          padding: EdgeInsets.all(10.0),
-                          child: Row(
-                            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                            children: <Widget>[
-                              Text("Proceed", style: TextStyle(fontSize: 24.0)),
-                            ],
-                          )),
-                      onPressed: () {
-                        if (_formKeyValue.currentState.validate()) {
-                          showAlertDialog(context);
-                        }
-                      },
-                      shape: new RoundedRectangleBorder(
-                          borderRadius: new BorderRadius.circular(30.0))),
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: RaisedButton(
+                        color: Colors.blue,
+                        textColor: Colors.white,
+                        child: Padding(
+                            padding: EdgeInsets.all(10.0),
+                            child: Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                              children: <Widget>[
+                                Text("Proceed",
+                                    style: TextStyle(fontSize: 24.0)),
+                              ],
+                            )),
+                        onPressed: () {
+                          if (_formKeyValue.currentState.validate()) {
+                            showAlertDialog(context);
+                          }
+                        },
+                        shape: new RoundedRectangleBorder(
+                            borderRadius: new BorderRadius.circular(30.0))),
+                  ),
                 ],
               ),
             ],
