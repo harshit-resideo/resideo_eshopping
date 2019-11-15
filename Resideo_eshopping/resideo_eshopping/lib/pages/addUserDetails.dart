e7e7c8c0a8fca6dc52aaad4f75713586f74860db
Pavan Lal Boro <pavan.boro@resideo.com>
2019-11-15 11:00:34 +0530

    screen3UI added

---
+++ Resideo_eshopping/resideo_eshopping/lib/pages/addUserDetails.dart
@@ -0,0 +1,190 @@
+import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+
+class addUserDetails extends StatelessWidget {
+  final GlobalKey<FormState> _formKeyValue = new GlobalKey<FormState>();
+
+  addUserDetails(this.estimatedCost);
+  final double estimatedCost;
+
+  showAlertDialog(BuildContext context) {
+    // set up the buttons
+    Widget cancelButton = FlatButton(
+      child: Text("No"),
+      onPressed: () {
+        //Todo: Do Something
+      },
+    );
+    Widget continueButton = FlatButton(
+      child: Text("Yes"),
+      onPressed: () {
+        //Todo: Do Something
+      },
+    );
+
+    // set up the AlertDialog
+    AlertDialog alert = AlertDialog(
+      title: Text("Order Confrimation"),
+      content: Text("Do you want to proceed?"),
+      actions: [
+        cancelButton,
+        continueButton,
+      ],
+    );
+
+    // show the dialog
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return alert;
+      },
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Container(
+          alignment: Alignment.center,
+          child: Text("Add User Details",
+              style: TextStyle(
+                color: Colors.white,
+              )),
+        ),
+      ),
+      body: SafeArea(
+        child: Form(
+          key: _formKeyValue,
+          autovalidate: true,
+          child: new ListView(
+            padding: const EdgeInsets.symmetric(horizontal: 15.0),
+            children: <Widget>[
+              TextFormField(
+                decoration: const InputDecoration(
+                  icon: const Icon(
+                    FontAwesomeIcons.userCircle,
+                    color: Colors.blue,
+                  ),
+                  hintText: 'Enter your Name',
+                  labelText: 'Name',
+                ),
+                validator: (val) {
+                  if (val.isEmpty) {
+                    return 'Name field should not be empty';
+                  } else {
+                    return null;
+                  }
+                },
+              ),
+              TextFormField(
+                decoration: const InputDecoration(
+                  icon: const Icon(
+                    FontAwesomeIcons.phone,
+                    color: Colors.blue,
+                  ),
+                  hintText: 'Ex: 9700000000',
+                  labelText: 'Phone',
+                ),
+                validator: (val) {
+                  Pattern pattern = r'^(?:[+0]9)?[0-9]{10}$';
+                  RegExp regex = new RegExp(pattern);
+                  if (!regex.hasMatch(val))
+                    return 'Enter Valid Phone Number';
+                  else
+                    return null;
+                },
+                keyboardType: TextInputType.number,
+              ),
+              TextFormField(
+                maxLines: 3,
+                decoration: const InputDecoration(
+                  icon: const Icon(
+                    FontAwesomeIcons.home,
+                    color: Colors.blue,
+                  ),
+                  labelText: 'Address',
+                ),
+                validator: (val) {
+                  if (val.isEmpty)
+                    return 'Address Field should not be empty';
+                  else
+                    return null;
+                },
+              ),
+              TextFormField(
+                decoration: const InputDecoration(
+                  icon: const Icon(
+                    FontAwesomeIcons.mapMarker,
+                    color: Colors.blue,
+                  ),
+                  hintText: 'Ex: 500000',
+                  labelText: 'Zip Code',
+                ),
+                validator: (val) {
+                  Pattern pattern = r'^[1-9][0-9]{5}$';
+                  RegExp regex = new RegExp(pattern);
+                  if (!regex.hasMatch(val))
+                    return 'Enter Valid ZipCode';
+                  else
+                    return null;
+                },
+                keyboardType: TextInputType.number,
+              ),
+              Row(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  Container(
+                    margin: const EdgeInsets.all(20.0),
+                    child: Text(
+                      'Estimated Price: ',
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 20,
+                        color: Colors.blue,
+                      ),
+                    ),
+                  ),
+                  Container(
+                    margin: const EdgeInsets.all(20.0),
+                    child: Text(
+                      'Rs. ' + estimatedCost.toString(),
+                      style:
+                          TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
+                    ),
+                  ),
+                ],
+              ),
+              SizedBox(
+                height: 200.0,
+              ),
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: <Widget>[
+                  RaisedButton(
+                      color: Colors.blue,
+                      textColor: Colors.white,
+                      child: Padding(
+                          padding: EdgeInsets.all(10.0),
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                            children: <Widget>[
+                              Text("Proceed", style: TextStyle(fontSize: 24.0)),
+                            ],
+                          )),
+                      onPressed: () {
+                        if (_formKeyValue.currentState.validate()) {
+                          showAlertDialog(context);
+                        }
+                      },
+                      shape: new RoundedRectangleBorder(
+                          borderRadius: new BorderRadius.circular(30.0))),
+                ],
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
